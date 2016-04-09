53d63dbe56f442f310bb43dd41af01462eb46894
Neel Kowdley <nkowdley@gmail.com>
2016-04-09 01:16:25 -0400

    more test

--- server/routes/getSong.js
+++ server/routes/getSong.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET getSong page. */
 router.get('/', function(req, res, next) {
-	var json = JSON.parse(body);
-	res.send("Hello World");
+	//var json = JSON.parse(body);
+	 //res.send(req.query[body]);
+	 console.log(req.query);
+	//res.send("Hello World"+);
  	//res.sendfile('../client/app/index.html');
  });
 
