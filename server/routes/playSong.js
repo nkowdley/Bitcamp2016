d1bfd2e0d3d1f109b92b9130b00d02ad0702dc49
jmm330 <jackmcquown@Jacks-MacBook-Air-2.local>
2016-04-09 17:34:28 -0400

    find() first song in db

--- server/routes/playSong.js
+++ server/routes/playSong.js
@@ -6,10 +6,22 @@ var db = mongoose.connection;
 /* GET getSong page. */
 router.get('/', function(req, res, next) {
 	console.log("here");
-	//db.collection('songs').findOne({}).sort{$min: 'time'}, {}, function(err, post) {
-	//	console.log(post);
-	//	res.send(post);
-	//});
+	// db.collection('songs').findOne({}).sort({$min: 'time'}), {}, function(err, post) {
+	// 	console.log(post);
+	// 	res.send(post);
+	// });
+	
+	db.collection('songs').find({}).toArray(function (err, song) {
+		if (err) {
+			console.log("Error: (err)");
+			return next(err);
+		}
+		console.log(song[0]);
+	});
+
+	// db.songs.findOne({}, function(err, song) {
+	// 	console.log(song);
+	// });
  });
 
 module.exports = router;
