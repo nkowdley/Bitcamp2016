ee42e482ac7024858378fcaba51a15955cddca32
Neel Kowdley <nkowdley@gmail.com>
2016-04-09 12:19:47 -0400

    fix brackets

--- server/routes/playSong.js
+++ server/routes/playSong.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var db=mongoose.connection;
+var db = mongoose.connection;
 
 /* GET getSong page. */
 router.get('/', function(req, res, next) {
-
 	
 	db.collection('songs').findOneandRemove({}, {}, { sort: { 'created_at' : 1} }, function(err, post) {
 		res.send(post);
-	}
-	
-	
+	});
+
+
 
 	//res.send("Hello World"+);
  	//res.sendfile('../client/app/index.html');
