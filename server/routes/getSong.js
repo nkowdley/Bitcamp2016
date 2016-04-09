74e9486488a11a5f2a2397e69b2bc25264962f1a
jmm330 <jackmcquown@Jacks-MacBook-Air-2.local>
2016-04-09 01:09:50 -0400

    reverted to old res.send

--- server/routes/getSong.js
+++ server/routes/getSong.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET getSong page. */
 router.get('/', function(req, res, next) {
 	var json = JSON.parse(body);
-	res.send("body");
+	res.send("Hello World");
  	//res.sendfile('../client/app/index.html');
  });
 
