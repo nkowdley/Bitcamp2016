189904a1dc0c2386d5e5ff0a77ee2754d34bf58a
Neel Kowdley <nkowdley@gmail.com>
2016-04-09 02:26:33 -0400

    add test route

--- server/routes/getSongtest.js
+++ server/routes/getSongtest.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 
-/* GET getSong page. */
+/*Test page in order to verify mongodb connection*/
 router.get('/', function(req, res, next) {
-  res.send(req.query.toString());
-  //res.sendfile('../client/app/index.html');
+	//Split the song string
+	var song = req.query["song"].split(' ').join('+');
+	db.collections('songs').insert(song);
+
 });
 
 module.exports = router;
