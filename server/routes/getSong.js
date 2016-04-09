df17ca6ca79719da9353da89bc5660d3b30cbfce
jmm330 <jackmcquown@Jacks-MacBook-Air-2.local>
2016-04-09 01:43:52 -0400

    test

--- server/routes/getSong.js
+++ server/routes/getSong.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
 
 /* GET getSong page. */
 router.get('/', function(req, res, next) {
 	//var json = JSON.parse(body);
 	 //res.send(req.query[body]);
-	 console.log(req.query);
+	 res.send(req.query["song"]);
+	 var song = req.query["song"];
 	//res.send("Hello World"+);
  	//res.sendfile('../client/app/index.html');
  });
