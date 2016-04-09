a3dac47639378500e04f04434447d7ed0a05f816
Neel Kowdley <nkowdley@gmail.com>
2016-04-09 02:34:34 -0400

    fix var db

--- server/routes/getSongtest.js
+++ server/routes/getSongtest.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
+var db=mongoose.connection;
 
 /*Test page in order to verify mongodb connection*/
 router.get('/', function(req, res, next) {
 	//Split the song string
 	var song = req.query["song"].split(' ').join('+');
 	db.collections('songs').insert(song);
-
 });
-
 module.exports = router;
