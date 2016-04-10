c6911ca025122f043003ccf0f3a1b4b5a57118ae
Neel Kowdley <nkowdley@gmail.com>
2016-04-09 22:29:02 -0400

    comment out a bad line

--- server/routes/playSong.js
+++ server/routes/playSong.js
@@ -4,20 +4,20 @@ var mongoose = require('mongoose');
 var db = mongoose.connection;
 
 /* GET getSong page. */
-router.get('/', function(req, res, next) {	
+router.get('/', function(req, res, next) {
 	//Find all the songs currently in the db, sort them oldest to newest
-	db.collection('songs').find({}).sort({created_at : -1}).toArray(function (err, song) {
+	db.collection('songs').find({}).sort({"time" : -1}).toArray(function (err, song) {
 		if (err) {
 			console.log("Error: (err)");
-			return next(err);
+			res.send(err);
 		}
+		console.log(song);
 		//Parse the JSON of the song
-		var songJSON = JSON.parse(song[0]);
-
+		//var songJSON = JSON.parse(song[0]);
+		db.collection('songs').remove(song[0]);
 		//Send the song JSON info
 		res.send(song[0]);
 		//Remove song from the db
-		db.collection('songs').remove(song[0]);
 	});
  });
 
